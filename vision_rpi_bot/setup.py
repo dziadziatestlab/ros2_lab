ff86f56f23251725966e72f6f001483dd0ae58bc
dziadzia <now_mi@wp.pl>
2024-12-29 14:48:32 +0100

    packege to new branch

--- vision_rpi_bot/setup.py
+++ vision_rpi_bot/setup.py
@@ -21,8 +21,8 @@ setup(
     
     entry_points={
         'console_scripts': [
-                'publisher_node = vision_rpi_bot.publisher:main',
-                'subscriber_node = vision_rpi_bot.subscriber:main',
+                'publisher_rpi_node = vision_rpi_bot.publisher:main',
+                'subscriber_rpi_node = vision_rpi_bot.subscriber:main',
         ],
     },
 )
